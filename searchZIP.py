c090ef8f02f47e2e1d1aa16f258ed9a7b9221fb1
Jesus Flores Guerra <chuyfloresguerra@gmail.com>
2025-12-31 12:11:37 -0700

    Add files via upload

---
+++ searchZIP.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import re, os
+
+path = "./logs2/"
+rodneys_library = os.listdir(path)
+myString = "key id"
+hit_count = 0
+
+for book in rodneys_library:
+    file = os.path.join(path, book)
+    with open(file, 'r') as inF:
+        for index, line in enumerate(inF):
+            if myString in line:
+                hit_count = hit_count + 1
+                print book + "|" + line,
+
+
+print " \n=> " + str(hit_count)
+#text.close()
